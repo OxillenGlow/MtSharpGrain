e1570e3342955d8e7d686470b6f8c7345bee6336
Oxillen Glow <oxillenglow@atomicmail.io>
2026-06-04 17:47:14 +0800

    ok but now it is white

--- src/main/java/com/mtsharpgrain/Main.java
+++ src/main/java/com/mtsharpgrain/Main.java
@@ -11,6 +11,7 @@ import com.jme3.system.JmeCanvasContext;
 import java.awt.Canvas;
 import java.io.IOException;
 import com.jvisualscripting.editor.VisualScriptingEditor;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
@@ -22,9 +23,46 @@ public class Main extends SimpleApplication {
 
     public static void main(String[] args) throws IOException {
         //I am seeing if embedding in jFrame works
-        Main app = new Main();
-        app.start();
+        System.out.println("0");
+        //Main app = new Main();
+        //app.start();
         // 1. Create the JFrame on the EDT
+        
+        Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+        System.out.println("0");
+        javax.swing.JFrame frame = new javax.swing.JFrame("Test");
+        System.out.println("1");
+        Main app = new Main();
+        System.out.println("2");
+        AppSettings settings = new AppSettings(true);
+        settings.setWidth(1280);
+        settings.setHeight(720);
+        System.out.println("3");
+        app.setSettings(settings);
+
+        app.createCanvas();
+        System.out.println("4");
+        JmeCanvasContext ctx =
+                (JmeCanvasContext) app.getContext();
+        System.out.println("5");
+        Canvas canvas = ctx.getCanvas();
+        System.out.println("6");
+        canvas.setSize(1280, 720);
+        System.out.println("7");
+        System.out.println("8");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        System.out.println("9");
+        frame.add(canvas);
+        System.out.println("10");
+        frame.pack();
+        System.out.println("11");
+        frame.setSize(1280, 720);
+        System.out.println("12");
+        frame.setVisible(true);
+        System.out.println("13");
+        app.startCanvas();
+        System.out.println("14");
+    
         
     }
 
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,33 +1,57 @@
 package test;
 
+import com.jme3.system.AppSettings;
+import com.jme3.system.JmeCanvasContext;
+import com.mtsharpgrain.Main;
+import java.awt.Canvas;
+import java.awt.Toolkit;
+import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-import org.lwjgl.system.Configuration;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //I am seeing if embedding in jFrame works
+        System.out.println("0");
+        //Main app = new Main();
+        //app.start();
         
-        Thread renderThread = new Thread(() -> {
-            SwingUtilities.invokeLater(() -> {
-    System.out.println("A");
+        
+        Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+        System.out.println("0");
+        javax.swing.JFrame frame = new javax.swing.JFrame("Test");
+        System.out.println("1");
+        Main app = new Main();
+        System.out.println("2");
+        AppSettings settings = new AppSettings(true);
+        settings.setWidth(1280);
+        settings.setHeight(720);
+        System.out.println("3");
+        app.setSettings(settings);
 
-    JFrame f = new JFrame("Test");
-
-    System.out.println("B");
-
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    System.out.println("C");
-
-    f.setSize(300, 300);
-
-    System.out.println("D");
-
-    f.setVisible(true);
-
-    System.out.println("E");
-});
-        }, "LWJGL-Render-Thread");
-        renderThread.start();
+        app.createCanvas();
+        System.out.println("4");
+        JmeCanvasContext ctx =
+                (JmeCanvasContext) app.getContext();
+        System.out.println("5");
+        Canvas canvas = ctx.getCanvas();
+        System.out.println("6");
+        canvas.setSize(1280, 720);
+        System.out.println("7");
+        System.out.println("8");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        System.out.println("9");
+        frame.add(canvas);
+        System.out.println("10");
+        frame.pack();
+        System.out.println("11");
+        frame.setSize(1280, 720);
+        System.out.println("12");
+        frame.setVisible(true);
+        System.out.println("13");
+        app.startCanvas();
+        System.out.println("14");
+    
+        
     }
 }
