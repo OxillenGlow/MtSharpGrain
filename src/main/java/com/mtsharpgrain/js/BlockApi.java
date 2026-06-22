4b26a22a87c3e9a4d0bc4f150dae9cc12dfb40b8
Oxillen Glow <oxillenglow@proton.me>
2026-06-22 16:11:49 +0800

    Create BlockApi.java

---
+++ src/main/java/com/mtsharpgrain/js/BlockApi.java
@@ -0,0 +1,33 @@
+package com.mtsharpgrain.js;
+
+import com.mtsharpgrain.RenderManager;
+import com.mtsharpgrain.WorldAccess;
+import org.graalvm.polyglot.HostAccess;
+
+public class BlockApi {
+
+    private final WorldAccess worldAccess;
+    private final RenderManager renderManager;
+
+    public BlockApi(WorldAccess worldAccess, RenderManager renderManager) {
+        this.worldAccess = worldAccess;
+        this.renderManager = renderManager;
+    }
+
+    @HostAccess.Export
+    public void placeBlock(int x, int y, int z, int blockId) {
+        worldAccess.setBlockAt(x, y, z, blockId);
+        renderManager.onBlockChanged(x, y, z);
+    }
+
+    @HostAccess.Export
+    public void destroyBlock(int x, int y, int z) {
+        worldAccess.removeBlockAt(x, y, z);
+        renderManager.onBlockChanged(x, y, z);
+    }
+
+    @HostAccess.Export
+    public int getBlock(int x, int y, int z) {
+        return worldAccess.getBlockAt(x, y, z);
+    }
+}
