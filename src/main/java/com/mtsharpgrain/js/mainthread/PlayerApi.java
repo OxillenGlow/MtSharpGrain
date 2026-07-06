e8d2452c2acdec859a2e85e16d0dd4ef66650c10
Oxillen Glow <oxillenglow@proton.me>
2026-07-06 12:20:53 +0800

    Modify PlayerApi to include rootNode for position calculations

--- src/main/java/com/mtsharpgrain/js/mainthread/PlayerApi.java
+++ src/main/java/com/mtsharpgrain/js/mainthread/PlayerApi.java
@@ -2,24 +2,30 @@ package com.mtsharpgrain.js.mainthread;
 
 import com.jme3.renderer.Camera;
 import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
 import org.graalvm.polyglot.HostAccess;
 
 public class PlayerApi {
 
     private final Camera cam;
+    private final Node rootNode;
 
-    public PlayerApi(Camera cam) {
+    public PlayerApi(Camera cam, Node rootNode) {
         this.cam = cam;
+        this.rootNode = rootNode;
     }
 
+    // cam.getLocation() is render-space (shifts with the floating origin).
+    // Scripts should only ever see/set true world coordinates.
     @HostAccess.Export
     public float[] getPosition() {
-        Vector3f p = cam.getLocation();
+        Vector3f p = cam.getLocation().subtract(rootNode.getLocalTranslation());
         return new float[]{ p.x, p.y, p.z };
     }
 
     @HostAccess.Export
     public void setPosition(float x, float y, float z) {
-        cam.setLocation(new Vector3f(x, y, z));
+        Vector3f trueWorldPos = new Vector3f(x, y, z);
+        cam.setLocation(trueWorldPos.add(rootNode.getLocalTranslation()));
     }
 }
