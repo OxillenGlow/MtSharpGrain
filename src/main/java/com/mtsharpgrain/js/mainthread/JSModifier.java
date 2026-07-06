f5099986ea7bf46cdb1401b91f1380d5750c22e2
Oxillen Glow <oxillenglow@proton.me>
2026-07-06 12:26:59 +0800

    Add rootNode parameter to JsApiBootstrap initialization

--- src/main/java/com/mtsharpgrain/js/mainthread/JSModifier.java
+++ src/main/java/com/mtsharpgrain/js/mainthread/JSModifier.java
@@ -50,7 +50,7 @@ public class JSModifier {
     public void init(AssetManager assetManager, Node rootNode, WorldAccess worldAccess, RenderManager renderManager, Camera cam) {
         WorldAccessor worldAccessor = new RealWorldAccessor(worldAccess);
         BlockApi blockApi = new BlockApi(worldAccess, renderManager);
-        this.bootstrap = new JsApiBootstrap(assetManager, worldAccessor, blockApi, cam);
+        this.bootstrap = new JsApiBootstrap(assetManager, worldAccessor, blockApi, cam, rootNode);
         this.bootstrap.getNodeRegistry().registerFixed(0L, rootNode);
     }
 
