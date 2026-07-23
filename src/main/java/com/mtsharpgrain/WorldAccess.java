915bdf000a6c4eeda447c0537f7a72f41668acaa
Oxillen Glow <oxillenglow@proton.me>
2026-07-23 14:09:50 +0800

    Enhance null check for unrendered chunk in setBlockAt
    Improve error handling when setting a block in an unrendered chunk.

--- src/main/java/com/mtsharpgrain/WorldAccess.java
+++ src/main/java/com/mtsharpgrain/WorldAccess.java
@@ -79,14 +79,19 @@ public final class WorldAccess {
     public void setBlockAt(int worldX, int worldY, int worldZ, int blockId) {
         ChunkPos chunkPos = worldToChunk(worldX, worldY, worldZ);
         BufferedChunk chunk = Useful.get(chunkPos);
-        //if (chunk == null) {System.out.println("Failed to break block at unrendered chunk: " + chunkPos);return;}
+        if (chunk == null) {
+            System.out.println("Failed to set block at unrendered chunk: " + chunkPos);
+            return;
+        }
         int localX = worldToLocal(worldX);
         int localY = worldToLocal(worldY);
         int localZ = worldToLocal(worldZ);
         try {
             modPackManager.notifyBlockSet(worldX, worldY, worldZ, blockId);
-        } catch (Exception e) {System.out.println("Placement rejected and handled:" + e);}
-        
+        } catch (Exception e) {
+            return;
+        }
+
         chunk.set(localX, localY, localZ, blockId);
     }
 
