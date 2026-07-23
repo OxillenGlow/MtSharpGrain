9db643d4d415cc1a48401b3a70eb85f73f2ba7df
Oxillen Glow <oxillenglow@proton.me>
2026-07-23 14:28:26 +0800

    Remove null check for chunk in setBlockAt
    Removed null check for chunk in setBlockAt method.

--- src/main/java/com/mtsharpgrain/WorldAccess.java
+++ src/main/java/com/mtsharpgrain/WorldAccess.java
@@ -79,10 +79,6 @@ public final class WorldAccess {
     public void setBlockAt(int worldX, int worldY, int worldZ, int blockId) {
         ChunkPos chunkPos = worldToChunk(worldX, worldY, worldZ);
         BufferedChunk chunk = Useful.get(chunkPos);
-        if (chunk == null) {
-            System.out.println("Failed to set block at unrendered chunk: " + chunkPos);
-            return;
-        }
         int localX = worldToLocal(worldX);
         int localY = worldToLocal(worldY);
         int localZ = worldToLocal(worldZ);
