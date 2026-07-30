47598acad2642585b6f31097a30ec726423bd20b
Oxillen Glow <oxillenglow@proton.me>
2026-07-30 12:08:34 +0800

    Refactor BlockApi to remove RenderManager
    Removed RenderManager dependency from BlockApi and updated methods to only use WorldAccess for block operations.

--- src/main/java/com/mtsharpgrain/js/BlockApi.java
+++ src/main/java/com/mtsharpgrain/js/BlockApi.java
@@ -1,35 +1,34 @@
 package com.mtsharpgrain.js;
 
-import com.mtsharpgrain.RenderManager;
 import com.mtsharpgrain.WorldAccess;
 import org.graalvm.polyglot.HostAccess;
 
+/**
+ * Exposes block operations to JS. Delegates all changes to WorldAccess only.
+ * WorldAccess is responsible for notifying the RenderManager when a change
+ * fully completes.
+ */
 public class BlockApi {
 
     private final WorldAccess worldAccess;
-    private final RenderManager renderManager;
 
-    public BlockApi(WorldAccess worldAccess, RenderManager renderManager) {
+    public BlockApi(WorldAccess worldAccess) {
         this.worldAccess = worldAccess;
-        this.renderManager = renderManager;
     }
 
     @HostAccess.Export
     public void placeBlock(int x, int y, int z, int blockId) {
         worldAccess.setBlockAt(x, y, z, blockId);
-        renderManager.onBlockChanged(x, y, z);
     }
 
     @HostAccess.Export
     public void destroyBlock(int x, int y, int z) {
         worldAccess.removeBlockAt(x, y, z);
-        renderManager.onBlockChanged(x, y, z);
     }
 
     @HostAccess.Export
     public void forceSetBlock(int x, int y, int z, int blockId) {
         worldAccess.forceSetBlockAt(x, y, z, blockId);
-        renderManager.onBlockChanged(x, y, z);
     }
 
     @HostAccess.Export
