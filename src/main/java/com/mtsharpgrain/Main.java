6573a0413274f3504200d58720745a67c6ec5080
Oxillen Glow <oxillenglow@proton.me>
2026-08-27 16:48:00 +0800

    Moved npc to its own pack

--- src/main/java/com/mtsharpgrain/Main.java
+++ src/main/java/com/mtsharpgrain/Main.java
@@ -313,9 +313,9 @@ public class Main extends SimpleApplication {
             AssetConverter.extract("/mods/teleport.js", "worlds/"+world+"/mod/TeleportMe/teleport.js");
             AssetConverter.extract("/mods/SurvivalFramework/03_death.js", "worlds/"+world+"/mod/SurvivalFramework/death.js");
             AssetConverter.extract("/mods/SurvivalFramework/05_location.js", "worlds/"+world+"/mod/SurvivalFramework/location.js");
-            AssetConverter.extract("/mods/simpleNPC.js", "worlds/"+world+"/mod/SurvivalFramework/simpleNPC.js");
+            AssetConverter.extract("/mods/simpleNPC.js", "worlds/"+world+"/mod/SimpleNPC/simpleNPC.js");
             AssetConverter.extract("/mods/TimerDemo.js", "worlds/"+world+"/mod/TimerTest/TimerDemo.js");
-            AssetConverter.extract("/mods/Delay.js", "worlds/"+world+"/mod/TimerTest/Delay.js");
+            AssetConverter.extract("/mods/Delay.js", "worlds/"+world+"/mod/TimerTest2/Delay.js");
            
             System.out.println("Extracted default mod files");
         } catch (IOException ex) {
